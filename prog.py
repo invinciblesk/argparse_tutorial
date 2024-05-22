67b4d2481ed4197c20d1ec15675bfd1242099111
invinciblesk <snzoka.sn@gmail.com>
2024-05-22 17:27:23 -0500

    modified prog.py to introduce count to count occurrences of specific options

--- prog.py
+++ prog.py
@@ -2,8 +2,8 @@
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("square", type=int,
-                    help="display a square of a given number")
-parser.add_argument("-v", "--verbosity", type=int,
+                    help="display the square of a given number")
+parser.add_argument("-v", "--verbosity", action="count",
                     help="increase output verbosity")
 args = parser.parse_args()
 answer = args.square**2
