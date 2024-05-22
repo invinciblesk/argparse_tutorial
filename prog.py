7f653fa610b76bdc2052b088ff1e387184f0a9c6
invinciblesk <snzoka.sn@gmail.com>
2024-05-22 16:57:45 -0500

    added arguments to prog.py

--- prog.py
+++ prog.py
@@ -1,3 +1,6 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.parse_args()
+parser.add_argument("square", help="display a square of a given number", type=int)
+args = parser.parse_args()
+print(args.square ** 2)
+
