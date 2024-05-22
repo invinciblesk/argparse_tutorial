d488a2cab6f90cf915955c950cb9ef0ff6cd3012
invinciblesk <snzoka.sn@gmail.com>
2024-05-22 17:37:44 -0500

    modified to set default value to 0 to prevent TypeError exception

--- prog.py
+++ prog.py
@@ -2,13 +2,11 @@
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("square", type=int,
-                    help="display the square of a given number")
-parser.add_argument("-v", "--verbosity", action="count",
+                    help="display a square of a given number")
+parser.add_argument("-v", "--verbosity", action="count", default=0,
                     help="increase output verbosity")
 args = parser.parse_args()
 answer = args.square**2
-
-# bugfix: replace == with >=
 if args.verbosity >= 2:
     print(f"the square of {args.square} equals {answer}")
 elif args.verbosity >= 1:
