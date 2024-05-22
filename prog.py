463e629087fdafebd953feb076c46bd3a6fd1bb8
invinciblesk <snzoka.sn@gmail.com>
2024-05-22 17:31:54 -0500

    modified prog.py to replace == with >= to make it verbose

--- prog.py
+++ prog.py
@@ -1,4 +1,4 @@
-# Combining Positional and Optional arguments
+
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("square", type=int,
@@ -7,9 +7,11 @@ parser.add_argument("-v", "--verbosity", action="count",
                     help="increase output verbosity")
 args = parser.parse_args()
 answer = args.square**2
-if args.verbosity == 2:
+
+# bugfix: replace == with >=
+if args.verbosity >= 2:
     print(f"the square of {args.square} equals {answer}")
-elif args.verbosity == 1:
+elif args.verbosity >= 1:
     print(f"{args.square}^2 == {answer}")
 else:
     print(answer)
